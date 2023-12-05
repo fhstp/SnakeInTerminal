ac3036979c096406c320d9fd03a6301a1df20667
Ramon Brullo <kingvanti650@gmail.com>
2023-12-05 18:57:34 +0100

    refactor: extract function

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,6 +2,7 @@ import com.googlecode.lanterna.TerminalSize
 import com.googlecode.lanterna.input.KeyStroke
 import com.googlecode.lanterna.input.KeyType
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
 
 fun tryDirectionFromKeyStroke(keyStroke: KeyStroke): Direction? {
     return when (keyStroke.character) {
@@ -13,20 +14,7 @@ fun tryDirectionFromKeyStroke(keyStroke: KeyStroke): Direction? {
     }
 }
 
-fun main(args: Array<String>) {
-    val gameSizeInput = args.getOrNull(0)
-        ?: throw Exception("Program is missing game-size argument")
-    val gameSize = gameSizeInput.toIntOrNull()
-        ?: throw Exception("Game-size argument is not an integer")
-
-    val snakeLengthInput = args.getOrNull(1)
-        ?: throw Exception("Program is missing snake-length argument")
-    val snakeLength = snakeLengthInput.toIntOrNull()
-        ?: throw Exception("Snake-length argument is not an integer")
-
-    val terminal = DefaultTerminalFactory()
-        .setInitialTerminalSize(TerminalSize(gameSize + 2, gameSize + 2))
-        .createTerminal()
+fun runGame(terminal: Terminal, gameSize: Int, snakeLength: Int) {
     val gfx = terminal.newTextGraphics()
 
     terminal.enterPrivateMode()
@@ -48,6 +36,24 @@ fun main(args: Array<String>) {
         printTo(gfx, game)
         terminal.flush()
     }
+}
+
+fun main(args: Array<String>) {
+    val gameSizeInput = args.getOrNull(0)
+        ?: throw Exception("Program is missing game-size argument")
+    val gameSize = gameSizeInput.toIntOrNull()
+        ?: throw Exception("Game-size argument is not an integer")
+
+    val snakeLengthInput = args.getOrNull(1)
+        ?: throw Exception("Program is missing snake-length argument")
+    val snakeLength = snakeLengthInput.toIntOrNull()
+        ?: throw Exception("Snake-length argument is not an integer")
+
+    val terminal = DefaultTerminalFactory()
+        .setInitialTerminalSize(TerminalSize(gameSize + 2, gameSize + 2))
+        .createTerminal()
+
+    runGame(terminal, gameSize, snakeLength)
 
     terminal.exitPrivateMode()
     terminal.close()
