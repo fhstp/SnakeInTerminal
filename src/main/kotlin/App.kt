89b39b61c1109b7997777f9b2c21882f792f902b
Ramon Brullo <kingvanti650@gmail.com>
2023-12-05 11:58:00 +0100

    feat: read game-size argument

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,14 @@
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import java.lang.Exception
 
-fun main() {
+fun main(args: Array<String>) {
+    val gameSizeInput = args.getOrNull(0)
+        ?: throw Exception("Program is missing game-size argument")
+    val gameSize = gameSizeInput.toIntOrNull()
+        ?: throw Exception("Game-size argument is not an integer")
+
+    println("Starting snake\n\tGame-size: $gameSize")
+
     val terminal = DefaultTerminalFactory().createTerminal()
     terminal.enterPrivateMode()
     terminal.putString("Hello world")
